dd4975a5443e9af9489351ebd002b1f59141de97
nrc97 <nog5fmox>
2019-06-22 21:09:27 +0000

    modif du samedi 22 juin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { ProfilComponent } from './profil/profil.component';
+import { AdminComponent } from './admin/admin.component';
+import { AuthGuardService } from './services/auth-guard.service';
+import { RegisterComponent } from './auth/register/register.component';
+import { ListeComponent } from './liste/liste.component';
+import { VoirProfilComponent } from './voir-profil/voir-profil.component';
+import { AdminGuardService } from './services/admin-guard.service';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'auth', component: AuthComponent},
+  {path: 'profil', canActivate: [AuthGuardService], component: ProfilComponent},
+  {path: 'admin',  canActivate: [AuthGuardService, AdminGuardService], component: AdminComponent},
+  {path: 'register/:type', canActivate: [AuthGuardService], component: RegisterComponent},
+  {path: 'register/add', canActivate: [AdminGuardService], component: RegisterComponent},
+  {path: 'liste', component: ListeComponent},
+  {path: 'mis-profil', component: VoirProfilComponent },
+  {path: '', component: AuthComponent},
+  {path: 'not-found', component: FourOhFourComponent},
+  {path: '**', redirectTo: '/not-found'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatButtonModule, MatToolbarModule, MatFormFieldModule, MatInputModule, MatDividerModule, MatExpansionModule, MatIconModule, MatCardModule, MatDialogModule, MatSelectModule, MatOptionModule} from '@angular/material';
+import { AuthComponent } from './auth/auth.component';
+import { ProfilComponent } from './profil/profil.component';
+import { AdminComponent } from './admin/admin.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ListeComponent } from './liste/liste.component';
+import { VoirProfilComponent } from './voir-profil/voir-profil.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    ProfilComponent,
+    AdminComponent,
+    HeaderComponent,
+    RegisterComponent,
+    ListeComponent,
+    VoirProfilComponent,
+    FourOhFourComponent
   ],
   imports: [
+    ReactiveFormsModule,
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatCardModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatOptionModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+  ]
 })
 export class AppModule { }
